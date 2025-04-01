5623ebdbd14564674ba994c0453184523cfc9007
ramenha0141 <ramenhaoishii@gmail.com>
2025-04-01 17:24:40 +0900

    infer

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,12 @@
-export type { InferColumnType, InferTableType } from './schema/types';
+import type {
+	AnyEnum,
+	AnyTable,
+	InferEnumType,
+	InferTableType,
+} from './schema/types';
+
+export type Infer<T> = T extends AnyTable
+	? InferTableType<T>
+	: T extends AnyEnum
+		? InferEnumType<T>
+		: never;
