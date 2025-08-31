718009a27ca0785d1fd600d0d60ae7010b8f7cff
ramenha0141 <ramenhaoishii@gmail.com>
2025-08-31 10:44:13 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { type DuckyOptions, type Ducky, createDucky } from './ducky';
 export * from './helper';
 
+export { DuckDBDataProtocol } from '@duckdb/duckdb-wasm';
 export * from 'kysely';
 export { datatypes } from '@coji/kysely-duckdb-wasm';
